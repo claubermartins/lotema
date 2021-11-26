40106d242d071b3defae069fb2050d5d71fa6576
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2021-11-26 17:55:36 -0300

    dividindo os arquivos components header e top-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopBarComponent } from './header/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
